47583e49365d1cf54f29178b30b5f799e8381e4d
sheepyis <zx9086@naver.com>
2023-11-21 17:44:13 +0900

    💄 대기실 UI 수정

--- CrazyArcade/src/crazyarcade/Waitroom.java
+++ CrazyArcade/src/crazyarcade/Waitroom.java
@@ -1,36 +1,60 @@
 package crazyarcade;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class Waitroom extends JFrame {
-	JScrollPane scrollPane;
-	ImageIcon icon;
-    Waitroom(){
-    	
-        super("대기실"); //타이틀
+    JScrollPane scrollPane;
+    ImageIcon icon;
+
+    Waitroom() {
+        super("대기실"); // Title
         JPanel jPanel = new JPanel();
 
-        icon = new ImageIcon("C:/Users/joie0/git/CrazyArcade/CrazyArcade/CrazyArcade/src/crazyarcade/image/waitroom.png");
-        //icon = new ImageIcon("C:/Users/user/git/CrazyArcade/CrazyArcade/src/CrazyArcade/image/login.jpg");
-        
-        JPanel background = new JPanel() {
+        icon = new ImageIcon("./image/waitroom.png");
+
+        JPanel background = new JPanel(new BorderLayout()) {
             @Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 if (icon != null) {
-                    g.drawImage(icon.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+                    int width = getWidth();
+                    int height = (int) (((double) width / icon.getIconWidth()) * icon.getIconHeight());
+                    g.drawImage(icon.getImage(), 0, 0, width, height, this);
                 }
             }
         };
-        
-        scrollPane = new JScrollPane(background);
-        setContentPane(scrollPane);
-        
-        setSize(900, 650);
-        add(jPanel);
 
+        scrollPane = new JScrollPane(jPanel);
+        scrollPane.setBorder(new LineBorder(Color.YELLOW, 3));
+
+        background.setBounds(0, 0, 900, 550);
+        scrollPane.setBounds(28, 555, 840, 200);
+
+        JTextField inputBox = new JTextField();
+        inputBox.setBounds(28, 760, 700, 35);
+
+        JButton button = new JButton();
+        button.setFocusPainted(false);
+        button.setText("보내기");
+        button.setBounds(735, 760, 131, 35);
+
+        getContentPane().add(background);
+        getContentPane().add(scrollPane);
+        getContentPane().add(inputBox);
+        getContentPane().add(button);
+
+        setLayout(null);
+
+        Color customColor = new Color(1, 49, 100);
+        getContentPane().setBackground(customColor);
+        setSize(900, 850);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Waitroom());
     }
 }
